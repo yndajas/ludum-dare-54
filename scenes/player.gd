6ef25f69eb54846e9401ff82a216e2be1cfe0546
Ynda Jas <yndajas@gmail.com>
2023-10-01 00:35:39 +0100

    Build out first section of level 1
    Also increase jump velocity to allow for more complex platforming

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -350.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
