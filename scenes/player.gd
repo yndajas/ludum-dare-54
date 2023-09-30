dd979e77beeaa0e5ed8e58314f1bce4692a72cd6
Ynda Jas <yndajas@gmail.com>
2023-09-30 23:14:33 +0100

    Refine player acceleration and deceleration

--- scenes/player.gd
+++ scenes/player.gd
@@ -21,8 +21,9 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		var target_velocity_x = direction * SPEED
+		velocity.x = move_toward(velocity.x, target_velocity_x, SPEED / 10)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED / 2)
 
 	move_and_slide()
