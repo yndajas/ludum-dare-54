2ee70ae4b2e7a455e8c41670dfa4a79b6a36965c
Ynda Jas <yndajas@gmail.com>
2023-09-30 02:50:44 +0100

    Restructure files
    This may change, but for now it adds an initial structure for files
    based on beginner's intuition

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -12,4 +12,4 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://level_1.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
