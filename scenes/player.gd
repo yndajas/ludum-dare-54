b76fba64db6a30066872f44f88f2eb7d823f1561
Ynda Jas <yndajas@gmail.com>
2023-09-30 23:42:48 +0100

    Remove double line breaks from scripts

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,12 +1,10 @@
 extends CharacterBody2D
 
-
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta: float) -> void:
 	set_animation()
