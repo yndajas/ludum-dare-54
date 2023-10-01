ae7214b47c11c095e72fa41bc4457c491b454b50
Ynda Jas <yndajas@gmail.com>
2023-10-01 17:34:31 +0100

    Move return to main menu handler to level 1 script

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -8,6 +8,10 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("return_to_main_menu"):
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
 func _on_door_1_body_entered(body: Node2D) -> void:
 	if body == $Player:
 		$Player.position = Vector2(-472, -664)
