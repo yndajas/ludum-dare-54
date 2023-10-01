76ae2c0bee5252a475eecaffd48b94367bda43bd
Ynda Jas <yndajas@gmail.com>
2023-10-01 21:23:06 +0100

    Queue section 2/3 music on door entered
    This lets the current track end before shifting to the next section's
    track

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
 @export var music_tracks: Array[AudioStreamWAV]
+var stream: AudioStreamWAV
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Music.stream = music_tracks[0]
-	$Music.play()
+	stream = music_tracks[0]
+	loop_music()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -21,7 +22,21 @@ func _input(event: InputEvent) -> void:
 func _on_door_1_body_entered(body: Node2D) -> void:
 	if body == $Player:
 		$Player.position = Vector2(-472, -664)
-
+		queue_track_change(music_tracks[1])
+	
 func _on_door_2_body_entered(body: Node2D) -> void:
 	if body == $Player:
 		$Player.position = Vector2(464, -448)
+		queue_track_change(music_tracks[2])
+
+func _on_music_finished() -> void:
+	loop_music()
+
+func loop_music() -> void:
+	$Music.stream = stream
+	$Music.stream.loop_mode = AudioStreamWAV.LOOP_FORWARD
+	$Music.play()
+
+func queue_track_change(new_track: AudioStreamWAV) -> void:
+	stream = new_track
+	$Music.stream.loop_mode = AudioStreamWAV.LOOP_DISABLED
