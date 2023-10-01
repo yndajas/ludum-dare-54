eca1219d44818ea16181edf048dcaadd6067bb7d
Ynda Jas <yndajas@gmail.com>
2023-10-01 20:50:34 +0100

    Add music
    - add three music tracks
    - play the first when the level is ready

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
+@export var music_tracks: Array[AudioStreamWAV]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Music.stream = music_tracks[0]
+	$Music.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
