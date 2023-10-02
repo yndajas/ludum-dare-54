0621bc8838992021b9b0a63cc399769a51ec9f9e
Ynda Jas <yndajas@gmail.com>
2023-10-02 01:19:20 +0100

    Optimise direction setting in player script
    Just get it once in the physics process instead of getting it in every
    helper method and having additional variables in two places

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,23 +6,22 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -350.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
+var direction: int = 0
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 var goal_reached: bool = false
 
 func _physics_process(delta: float) -> void:
+	direction = Input.get_axis("move_left", "move_right")
 	goal_reached = get_parent().goal_reached
 	set_animation()
 	set_orientation()
 	set_movement(delta)
 	move_and_slide()
 
-func get_direction() -> int:
-	return Input.get_axis("move_left", "move_right")
-
 func set_animation() -> void:
 	if not is_on_floor():
 		$AnimatedSprite2D.play("jump_middle")
-	elif get_direction() && !goal_reached:
+	elif direction && !goal_reached:
 		$AnimatedSprite2D.play("run")
 	else:
 		$AnimatedSprite2D.play("idle")
@@ -34,7 +33,6 @@ func set_movement(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 		play_jump_sfx()
 
-	var direction := get_direction()
 	if direction && !goal_reached:
 		var target_velocity_x := direction * SPEED
 		velocity.x = move_toward(velocity.x, target_velocity_x, SPEED / 10)
@@ -42,7 +40,6 @@ func set_movement(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED / 2)
 
 func set_orientation() -> void:
-	var direction := get_direction()
 	if direction && !goal_reached:
 		if direction == -1:
 			$AnimatedSprite2D.flip_h = true
